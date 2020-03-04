d38556932e9cc1bc0104f5a0764b73a78a4f1cd1
Sabbagh <as@adn-realty.com>
2020-03-04 13:39:11 +0100

    add: webpack

--- build/model/buttonModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Button = (function () {
-    function Button() {
-    }
-    return Button;
-}());
-exports.Button = Button;
-//# sourceMappingURL=buttonModel.js.map
